7435f7385701f6758d01647beb14534d82e2709f
Grant Davis <grantdavis303@gmail.com>
2026-02-04 15:09:18 -0700

    Update sticky note styles and content

--- client/src/components/StickyNote.tsx
+++ client/src/components/StickyNote.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { PencilSquareIcon, TrashIcon, EllipsisHorizontalIcon } from '@heroicons/react/24/outline'
+import { PencilSquareIcon, TrashIcon, EllipsisHorizontalIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import './StickyNote.css'
 
 interface Props {
@@ -15,24 +15,17 @@ interface Props {
 
 function StickyNote({ id, title, content, color, created_at, updated_at, onDelete, onUpdate }: Props) {
   const [showIcons, setShowIcons] = useState(false);
-  // const [dropdownStatus, setDropdownStatus] = useState('closed');
 
   function formatDate(date: string) {
     const dateObj = new Date(date);
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-    return `${days[dateObj.getDay()]}, ${months[dateObj.getMonth()]} ${dateObj.getDate()}, ${dateObj.getFullYear()}`;
+    return `${days[dateObj.getDay()]}, ${months[dateObj.getMonth()]} ${dateObj.getDate()}, ${dateObj.getFullYear()} at ${dateObj.toLocaleTimeString()}`;
   }
 
   const handleEllipsisClick = () => {
     setShowIcons(prev => !prev);
-
-    // if (dropdownStatus === 'closed') {
-    //   setDropdownStatus('open');
-    // } else {
-    //   setDropdownStatus('closed');
-    // }
   }
 
   return (
@@ -41,25 +34,25 @@ function StickyNote({ id, title, content, color, created_at, updated_at, onDelet
         <h3 style={{ marginTop: 'unset'}}>{title}</h3>
 
         <div style={{ margin: 'unset', padding: 'unset', display: 'flex', flexDirection: 'column', gap: '0.5rem'}}>
-          <EllipsisHorizontalIcon className="Icon Update" onClick={handleEllipsisClick} />
 
+        <EllipsisHorizontalIcon className={`Icon Overflow ${showIcons ? 'Closed' : 'Open'}`} onClick={handleEllipsisClick} />
+        <XMarkIcon className={`Icon Close ${showIcons ? 'Open' : 'Closed'}`} onClick={handleEllipsisClick} />
 
+        {/*
+          <EllipsisHorizontalIcon className={`Icon ${showIcons ? 'Open' : 'Closed'}`} onClick={handleEllipsisClick} />
+          <XMarkIcon className={`Icon ${showIcons ? 'Open' : 'Closed'}`} onClick={handleEllipsisClick} />
+        */}
         </div>
       </div>
 
       <p style={{ whiteSpace: 'pre-wrap' }}>{content}</p>
       <p className='mini'> Created {formatDate(created_at)} </p>
+      {/* <p className='mini'> Updated {formatDate(updated_at)} </p> */}
 
-        {/* { showIcons && ( */}
-          <div className={`IconDropdownContainer ${showIcons ? 'open' : 'closed'}`}>
-            <PencilSquareIcon className="Icon Update" onClick={() => onUpdate(id)} />
-            <TrashIcon className="Icon Delete" onClick={() => onDelete(id)} />
-          </div>
-        {/* )} */}
-
-      {/* <p className='small'> Updated {formatDate(updated_at)} </p> */}
-      {/* <button style={{ padding: '0.4rem', backgroundColor: 'red' }} onClick={() => onDelete(id)}> Delete </button>
-      <button style={{ padding: '0.4rem', backgroundColor: 'blue' }} onClick={() => onUpdate(id)}> Update </button> */}
+      <div className={`IconDropdownContainer ${showIcons ? 'open' : 'closed'}`}>
+        <PencilSquareIcon className="Icon Update" onClick={() => onUpdate(id)} />
+        <TrashIcon className="Icon Delete" onClick={() => onDelete(id)} />
+      </div>
     </div>
   )
 }
